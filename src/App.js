4ea56275d97f822b347240efc4ab5d1430610456
nurhossainfarid <faahsan1516@gmail.com>
2022-03-16 09:40:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Farid></Farid>
+      <Farid></Farid>
+      <Farid></Farid>
+      <Farid></Farid>
+      <Abid></Abid>
     </div>
   );
+};
+const Farid = () => {
+  return (
+    <div className='farid-style'>
+      <h2>Nur Hossain Farid</h2>
+      <p>Job: Student of DIU</p>
+    </div>
+  );
+}; 
+const Abid = () => {
+  return (
+    <div className='abid-style'>
+      <h2>Shariya Alam Abid</h2>
+      <p>Job: Student of TM</p>
+    </div>
+  )
 }
 
 export default App;
